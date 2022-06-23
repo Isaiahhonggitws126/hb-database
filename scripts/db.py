7ab0d3be03a012594935d531d8d6576e854a4c10
Isaiah Hong <34777406+Isaiahhonggitws126@users.noreply.github.com>
2022-06-23 12:05:49 -0400

    updated files

--- scripts/db.py
+++ scripts/db.py
@@ -1,5 +1,6 @@
 import psycopg2 as ps
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from pathlib import Path
 from sqlalchemy import INTEGER, TEXT
@@ -14,6 +15,9 @@ user=os.getenv('user')
 password=os.getenv('password')
 
 def connect_to_db():
+
+    conn = None
+    
     try:
        conn = ps.connect(
            host=host,
@@ -43,3 +47,88 @@ def create_table(curr):
     
     curr.execute(create_table_command)
     print("Table Created!")
+
+
+def check_if_video_exists(curr, channel_id):
+    query = ("""SELECT channel_id FROM channels WHERE channel_id = %s""")
+    curr.execute(query, (channel_id,))
+    
+    return curr.fetchone() is not None
+
+
+def update_row(curr, channel_id, channel_title, description, playlist_id, published_at, title, thumbnails_default_url, thumbnails_high_url, thumbnails_standard_url):
+
+    query = ("""UPDATE channels
+                SET channel_id = %s,
+                channel_title = %s,
+                description = %s,
+                playlist_id = %s,
+                published_at = %s,
+                title = %s,
+                thumbnails_default_url = %s,
+                thumbnails_high_url = %s,
+                thumbnails_standard_url = %s 
+                """)
+    cols_to_update = (channel_id, channel_title, description, playlist_id, published_at, title, thumbnails_default_url, thumbnails_high_url, thumbnails_standard_url)
+    curr.execute(query, cols_to_update)
+
+def insert_into_table(curr, channel_id, channel_title, description, playlist_id, published_at, title, thumbnails_default_url, thumbnails_high_url, thumbnails_standard_url):
+    insert_into_channels = ("""INSERT INTO channels (channel_id, 
+                                                    channel_title, 
+                                                    description, 
+                                                    playlist_id, 
+                                                    published_at, 
+                                                    title, 
+                                                    thumbnails_default_url, 
+                                                    thumbnails_high_url, 
+                                                    thumbnails_standard_url)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+                            """)
+    fields_to_update = (channel_id, channel_title, description, playlist_id, published_at, title, thumbnails_default_url, thumbnails_high_url, thumbnails_standard_url)            
+    curr.execute(insert_into_channels, fields_to_update)
+
+def truncate_table(curr):
+    truncate_table = ("""TRUNCATE TABLE channels""")
+    curr.execute(truncate_table)
+
+def df_to_db(curr, df):
+    for i, row in df.iterrows():
+        insert_into_table(curr, 
+                          row['channel_id'],
+                          row['channel_title'],
+                          row['description'],
+                          row['playlist_id'],
+                          row['published_at'],
+                          row['title'],
+                          row['thumbnails_default_url'],
+                          row['thumbnails_high_url'],
+                          row['thumbnails_standard_url'])
+
+def update_db(curr, df):
+    tmp_df = pd.DataFrame(columns=['channel_id', 
+                                   'channel_title', 
+                                   'description', 
+                                   'playlist_id', 
+                                   'published_at', 
+                                   'title', 
+                                   'thumbnails_default_url', 
+                                   'thumbnails_high_url', 
+                                   'thumbnails_standard_url'])
+
+    for i, row in df.iterrows():
+        if check_if_video_exists(curr, row['channel_id']):
+            update_row(curr, 
+                        row['channel_id'],
+                        row['channel_title'],
+                        row['description'],
+                        row['playlist_id'],
+                        row['published_at'],
+                        row['title'],
+                        row['thumbnails_default_url'],
+                        row['thumbnails_high_url'],
+                        row['thumbnails_standard_url'])
+
+        else:
+            tmp_df = tmp_df.append(row)
+        return tmp_df
+            
--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,11 @@ import pandas as pd
 import time
 from dotenv import load_dotenv
 from pathlib import Path
+import os
 
 # imports from other scripts
 from yt_api import get_channel_id, channel_details, video_details, formatted_print, channel_id_dict
-from db import create_table, connect_to_db
+from db import connect_to_db, df_to_db, update_db
 
 # environment variable
 load_dotenv(Path("../.env"))
@@ -22,7 +23,7 @@ def extract_api_data():
         formatted_print(videos_list)
     return
 
-def transform():
+def load():
     api_response = pd.read_json("json-data/json-response.json")
 
     # Fetching YT metadata.
@@ -40,11 +41,33 @@ def transform():
                             'thumbnails.high.url': 'thumbnails_high_url',
                             'thumbnails.standard.url': 'thumbnails_standard_url',
                            }) 
-                                    
-    return youtube_data
-        
+
+
+    # Fetches the columns we only need for our DB.
+    youtube_data = youtube_data[['channel_id', 
+                                'channel_title', 
+                                'description',
+                                'playlist_id',
+                                'published_at',
+                                'title',
+                                'thumbnails_default_url',
+                                'thumbnails_high_url',
+                                'thumbnails_standard_url'
+                                ]]          
+    
+    # Loading into DB
+    conn = connect_to_db()
+    curr = conn.cursor()
+
+    new_channel_df = update_db(curr, youtube_data)
+
+    df_to_db(curr, new_channel_df)
+
+    conn.commit()
+
+            
 def main():
-    print(transform())
+    print(load())
    
 
 if __name__ == '__main__':
